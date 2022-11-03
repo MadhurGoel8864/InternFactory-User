b41b099d26864ae0378b2eef7b7a50d0c9e63b4c
Madhur <madhur12a16072@gmail.com>
2022-11-03 23:19:08 +0530

    Navigation Bar Added

--- app/src/main/java/com/example/internfactory/activity_Dashboard.kt
+++ app/src/main/java/com/example/internfactory/activity_Dashboard.kt
@@ -1,13 +1,21 @@
 package com.example.internfactory
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.Profile
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
+import com.example.internfactory.Activities.Auth.SignIn_Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class activity_Dashboard : AppCompatActivity() {
+
+    private lateinit var bottom_nav: BottomNavigationView
+
+    var count=1;
 
     private fun replaceFrag(fragment : Fragment, name: String){
         val fm : FragmentManager =supportFragmentManager
@@ -35,6 +43,19 @@ class activity_Dashboard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        bottom_nav = findViewById(R.id.bottomnavbar)
+        bottom_nav.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.Home ->  replaceFrag(DashBoard_Fragment(),"Home")
+                R.id.Profile -> replaceFrag(ProfileSection_Fragment(),"Profile")
+                R.id.Search -> replaceFrag(trendingSeeAll(),"Search")
+                R.id.Applied -> replaceFrag(CategoriesSeeAll(),"Applied")
+                else->{
+                }
+            }
+            true
+        }
         val fm : FragmentManager = supportFragmentManager
         val ft : FragmentTransaction = fm.beginTransaction()
         val logInFragment = DashBoard_Fragment()
