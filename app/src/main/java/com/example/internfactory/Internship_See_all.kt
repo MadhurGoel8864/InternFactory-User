ccc5767074422983dbeb59018dcc8deee65270e8
Naman Bansal <bansalnaman44@gmail.com>
2022-11-13 14:53:14 +0530

    All internship added

--- app/src/main/java/com/example/internfactory/Internship_See_all.kt
+++ app/src/main/java/com/example/internfactory/Internship_See_all.kt
@@ -8,7 +8,9 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.internfactory.Activities.Adapters.trending_seeall_adapters
+import com.example.internfactory.Activities.Adapters.internship_adapter
+import com.example.internfactory.modules.Internship_request
+import com.example.internfactory.modules.Internship_response
 import com.example.internfactory.modules.trending_seeall_response
 import com.example.internfactory.server.RetrofitApi
 import com.example.internfactory.server.ServiceBuilder
@@ -18,7 +20,7 @@ import javax.security.auth.callback.Callback
 
 class Internship_See_all : Fragment() {
 
-//    lateinit var adapter: internship_adapter
+    lateinit var adapter: internship_adapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -26,38 +28,38 @@ class Internship_See_all : Fragment() {
         // Inflate the layout for this fragment
         val view= inflater.inflate(R.layout.fragment_internship__see_all, container, false)
 
-//        val recyclerView = view.findViewById<RecyclerView>(R.id.MyInternshiprecyclerview)
-//
-//        val serviceBuilder = ServiceBuilder.buildService(RetrofitApi::class.java)
-//        val Call = serviceBuilder.gettrends("Bearer " + (activity as activity_Dashboard).token)
-//
-//        Call.enqueue(object : Callback,
-//            retrofit2.Callback<MutableList<trending_seeall_response>> {
-//            override fun onResponse(
-//                call: Call<MutableList<trending_seeall_response>>,
-//                response: Response<MutableList<trending_seeall_response>>
-//            ) {
-//                if(response.isSuccessful) {
-//                    recyclerView.layoutManager= LinearLayoutManager(requireContext())
-//                    adapter= internship_adapter(response.body()!!)
-//                    recyclerView.adapter=adapter
-//                    adapter.setOnItemClickListner(object : internship_adapter.onItemClickListner {
-//                        override fun onItemClick(position: Int) {
-//                            Toast.makeText(view?.context,"Clicked Item", Toast.LENGTH_SHORT).show()
-//                        }
-//                    })
-//                }
-//            }
-//
-//            override fun onFailure(
-//                call: Call<MutableList<trending_seeall_response>>,
-//                t: Throwable
-//            ) {
-//                t.printStackTrace()
-//            }
-//
-//        })
+        val recyclerView = view.findViewById<RecyclerView>(R.id.MyInternshiprecyclerview)
+
+        val internshipRequest=Internship_request("0","5","id","asc")
+        val serviceBuilder = ServiceBuilder.buildService(RetrofitApi::class.java)
+        val Call = serviceBuilder.allinternship(1,internshipRequest,"Bearer " + (activity as activity_Dashboard).token)
+
+        Call.enqueue(object : Callback,
+            retrofit2.Callback<Internship_response> {
+            override fun onResponse(
+                call: Call<Internship_response>,
+                response: Response<Internship_response>
+            ) {
+                if(response.isSuccessful) {
+                    recyclerView.layoutManager= LinearLayoutManager(requireContext())
+                    adapter= internship_adapter(response.body()!!)
+                    recyclerView.adapter=adapter
+                    adapter.setOnItemClickListner(object : internship_adapter.onItemClickListner {
+                        override fun onItemClick(position: Int) {
+                            Toast.makeText(view?.context,"Clicked Item", Toast.LENGTH_SHORT).show()
+                        }
+                    })
+                }
+            }
+
+            override fun onFailure(
+                call: Call<Internship_response>,
+                t: Throwable
+            ) {
+                t.printStackTrace()
+            }
+
+        })
         return view
     }
-//
 }
