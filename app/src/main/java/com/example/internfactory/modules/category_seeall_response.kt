3903f9067b92f47e09a0f1de93c1ee68885604dd
Naman Bansal <bansalnaman44@gmail.com>
2022-11-14 17:41:45 +0530

    Card design changes

--- app/src/main/java/com/example/internfactory/modules/category_seeall_response.kt
+++ app/src/main/java/com/example/internfactory/modules/category_seeall_response.kt
@@ -1,9 +1,5 @@
 package com.example.internfactory.modules
 
-import android.os.Parcel
-import android.os.Parcelable
-
-
 data class category_seeall_response(
     val categoryId: Int?,
     val categoryName: String?,
