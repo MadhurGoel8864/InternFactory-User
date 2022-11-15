7b72d223f25ceddfaa3c554beac69c90ab477c40
Madhur <madhur12a16072@gmail.com>
2022-11-15 13:28:42 +0530

    internship details card view edited

--- app/src/main/java/com/example/internfactory/Activities/Adapters/Internship_Adapter.kt
+++ app/src/main/java/com/example/internfactory/Activities/Adapters/Internship_Adapter.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -26,7 +27,7 @@ class internship_adapter(val internshipResponse: Internship_response):
 
     @SuppressLint("SuspiciousIndentation")
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): internshipViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.card_trending_seeall,parent,false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.card_all_internship,parent,false)
             return internshipViewHolder(view,mListner)
     }
 
@@ -40,15 +41,27 @@ class internship_adapter(val internshipResponse: Internship_response):
 
     class internshipViewHolder(itemView: View,listner: onItemClickListner): RecyclerView.ViewHolder(itemView){
 
-        private val trending_title: TextView = itemView.findViewById(R.id.trending_title)
-        private val trending_image: ImageView = itemView.findViewById(R.id.trending_image)
+//        private val trending_title: TextView = itemView.findViewById(R.id.trending_title)
+//        private val trending_image: ImageView = itemView.findViewById(R.id.trending_image)
+        private val company_logo:ImageView = itemView.findViewById(R.id.imageView14)
+        private val internship_domain:TextView = itemView.findViewById(R.id.textView26)
+        private val company_name:TextView = itemView.findViewById(R.id.textView27)
+        private val remote_location:TextView = itemView.findViewById(R.id.textView28)
+        private val internship_timeline:TextView = itemView.findViewById(R.id.textView29)
+        private val amount:TextView = itemView.findViewById(R.id.textView30)
+        private val apply_btn:Button = itemView.findViewById(R.id.Apply_btn)
+
 
 
         fun bindView(internshipSeeallResponse: Internship_response){
-            trending_title.text = internshipSeeallResponse.content[0].title
-            val x =
-                "https://internfactory.herokuapp.com/file/" + internshipSeeallResponse.content[0].imageUrl
-            trending_image.load(x)
+//            trending_title.text = internshipSeeallResponse.content[0].title
+            internship_domain.text = internshipSeeallResponse.content[0].title
+            company_name.text = internshipSeeallResponse.content[0].category.categoryName
+            remote_location.text = internshipSeeallResponse.content[0].type
+            internship_timeline.text = internshipSeeallResponse.content[0].tenure
+            amount.text = internshipSeeallResponse.content[0].stipend
+            val x = "https://internfactory.herokuapp.com/file/" + internshipSeeallResponse.content[0].imageUrl
+            company_logo.load(x)
         }
 
         init{
